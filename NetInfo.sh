814c12ca3db613bf37a7d89de361522a5d50c9e6
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2026-08-10 10:25:24 +0200

    Add files via upload

---
+++ NetInfo.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Banniere
+cat <<"EOF"
+
+_______________________________________________
+7     77     77      77  77     77     77     7
+|  _  ||  ___!!__  __!|  ||  _  ||  ___!|  7  |
+|  7  ||  __|_  7  7  |  ||  7  ||  __| |  |  |
+|  |  ||     7  |  |  |  ||  |  ||  7   |  !  |
+!__!__!!_____!  !__!  !__!!__!__!!__!   !_____!
+                                                                                       
+by M0rPH3U53
+      
+EOF
+
+# Couleur ASSCI
+BLEU='\033[34m'
+ROUGE='\033[0;31m'
+VERT='\033[0;32m'
+GRIS='\033[0;90m'
+RESET='\033[0m'
+BLANC='\033[1;37m'
+JAUNE='\033[0;33m'
+
+# Affiche interfaces avec adresse réseaux
+interfaces=$(ip route | grep -E '[0-9]' | grep -v default | awk '{print $3 " --> " $1}')
+
+# Recupere l'ip de machine
+IP_hote=$(ip route | grep -E '[0-9]' | grep -v default | awk '{print $9}')
+
+# Recupere l'adresse du serveur dns
+dns_serveur=$(cat /etc/resolv.conf | grep -v Generated | awk '{print $2}')
+
+# Rcupere ip  passerelle par defaut
+gateway_ip=$(ip route show default | awk '{print $3}')
+
+echo " "
+echo "${BLEU}[*]${RESET} ${BLANC}Interfaces ${RESET} "
+echo " "
+echo "${interfaces}"
+echo " "
+echo "${BLEU}[*]${RESET} ${BLANC}IP ${RESET} "
+echo " "
+echo "${IP_hote}"
+echo " "
+echo "${BLEU}[*]${RESET} ${BLANC}DNS ${RESET} "
+echo " "
+echo "${dns_serveur}"
+echo " "
+echo "${BLEU}[*]${RESET} ${BLANC}Passerelle ${RESET} "
+echo " "
+echo "${gateway_ip}"
+echo " "
