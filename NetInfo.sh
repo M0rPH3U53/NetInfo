5a0fa6548e560d1b4d6f6b3e4950f856405b361a
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2026-08-14 11:02:45 +0200

    Update NetInfo.sh

--- NetInfo.sh
+++ NetInfo.sh
@@ -30,7 +30,7 @@ hostename=$(hostname)
 interfaces=$(ip route | grep -E '[0-9]' | grep -v default | awk '{print $3 " --> " $1}')
 
 # Broadcast
-broadcast=$(ifconfig | grep inet | grep -v inet6 | awk '{print $2 " --> " $6}')
+broadcast=$(ifconfig | grep inet | grep -v inet6 | grep -v  "inet 127.0.0.1" | awk '{print $2 " --> " $6}')
 
 # Ports ouvert
 ports=$(ss -lpnH -4 2>/dev/null | awk '{print $5 " --> " $7}')
